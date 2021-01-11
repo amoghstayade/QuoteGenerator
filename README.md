c75ceae223c3269370077510c4db73a1c9c3886b
Amogh Tayade <amogh.tayade@cbre.com>
2021-01-11 10:50:02 -0600

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # QuoteGenerator
+
+Quote Generator is a web app that displays you a personalized Quote along with other widgets.
+Technologies used - 
+1. Python
+2. Flask
+3. REST API
---
+++ src/quoteclient.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import random
+import logging
+import sys
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    stream=sys.stdout,
+    level=logging.INFO,
+)
+
+def get_quote():
+    url = "https://type.fit/api/quotes"
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    quotes = json.loads(response.text)
+    my_quote = quotes[random.randrange(len(quotes)-1)]
+    logger.info(my_quote)
+    return my_quote["text"], my_quote["author"]
+
+def get_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    # date_today = now.
+
+quote, author = get_quote()
+print(quote, author)
